d15e1140d1d8adc76c666976c194dbe0688e9a31
gent.rexha <gent.rexha@gmail.com>
2019-08-31 16:50:29 +0200

    Finished Infographic.

---
+++ src/kosovo_election_results.R
@@ -0,0 +1,39 @@
+install.packages("pacman")
+pacman::p_load(ggparliament, dplyr, ggplot2)
+
+# set wd
+setwd("C:/Projects/Personal/kosovo-past-elections/src/")
+
+election_data <- read.csv2("../data/interim/election-results.csv",stringsAsFactors=FALSE)
+
+election_2017_data <- election_data %>%
+  filter(year == 2017)
+
+# use descending percentage (-percentage)
+election_2017_data <- transform(election_2017_data, ï..party_short = reorder(ï..party_short, -percentage))
+
+election_2019_data <- election_data %>%
+  filter(year == 2019)
+
+# use descending votes (-votes)
+election_2019_data <- transform(election_2019_data, ï..party_short = reorder(ï..party_short, -percentage))
+ 
+ggplot(data=election_2017_data, aes(x=ï..party_short, y=percentage, fill=ï..party_short)) +
+  labs(title="Rezultatet e zgjedhjeve të 2017-ës sipas koalicioneve të 2019-ës", x="Partia ose Koalicionet",
+       y="Përqindja e votave të përgjithshme", fill="Partia ose Koalicionet")+
+  geom_bar(stat="identity") +
+  theme_minimal() +
+  scale_fill_manual(values = election_2017_data$colour, limits = election_2017_data$party_short)
+
+# save
+ggsave("visualization/election-results-percentage-summarized-2017-shqip.png")
+
+ggplot(data=election_2019_data, aes(x=ï..party_short, y=percentage, fill=ï..party_short)) +
+  labs(title="Rezultatet e zgjedhjeve të 2017-ës sipas koalicioneve të 2019-ës", x="Partia ose Koalicionet",
+       y="Përqindja e votave të përgjithshme", fill="Partia ose Koalicionet")+
+  geom_bar(stat="identity") +
+  theme_minimal() +
+  scale_fill_manual(values = election_2019_data$colour, limits = election_2019_data$party_short)
+
+# Save
+ggsave("visualization/election-results-percentage-summarized-2019-shqip.png")
--- src/kosovo_parlament.R
+++ src/kosovo_parlament.R
@@ -4,29 +4,57 @@ pacman::p_load(ggparliament, dplyr, ggplot2)
 # set wd
 setwd("C:/Projects/Personal/kosovo-past-elections/src/")
 
-ks_data <- read.csv2("../data/interim/parlamenti_2017.csv")
+kosovo_parliament_data <- read.csv2("../data/interim/parlamenti_summarized.csv",stringsAsFactors=FALSE)
 
-ks_parliament <- parliament_data(election_data = ks_data,
+ksp_2017_data <- kosovo_parliament_data %>%
+  filter(ï..year == 2017)
+  
+ksp_2019_data <- kosovo_parliament_data %>%
+  filter(ï..year == 2019)
+
+ks_parliament_2017 <- parliament_data(election_data = ksp_2017_data,
                                  type = "semicircle",
                                  parl_rows = 5,
-                                 party_seats = ks_data$seats)
+                                 party_seats = ksp_2017_data$seats)
 
-deputies <- ggplot(ks_parliament, aes(x, y, colour = party_short)) +
-  geom_parliament_seats() + 
+deputies_2017 <- ggplot(ks_parliament_2017, aes(x, y, colour = party_short)) +
+  geom_parliament_seats(size=5) + 
   #highlight the party in control of the House with a black line
   geom_highlight_government(government == 1) +
   #draw majority threshold
-  draw_majoritythreshold(n = 61, label = TRUE, type = 'semicircle')+
+  # draw_majoritythreshold(n = 61, label = TRUE, type = 'semicircle')+
   #set theme_ggparliament
   theme_ggparliament() +
   #other aesthetics
-  labs(colour = NULL, 
-       title = "Republic of Kosovo Parliament",
-       subtitle = "Party that controls the Parliament highlighted.") 
-#+
-#  scale_colour_manual(values = us_house$colour, 
-#                      limits = us_house$party_short) 
+  labs(colour = "Partia ose Koalicionet", 
+       title = "Parlamenti i Republikes së Kosovës në vitin 2017",
+       subtitle = "Ulëset e deputetëve sipas zgjedhjeve të 2017-ës dhe koalicioneve të 2017-ës") +
+  scale_colour_manual(values = ks_parliament_2017$colour, limits = ks_parliament_2017$party_short)
 
-deputies
+deputies_2017
 
-ggsave("visualization/kosovo-parliament-v3.png", width=12, height = 7)
+ggsave("visualization/kosovo-parliament-summarized-2017-shqip.png", width=12, height = 7)
+
+ks_parliament_2019 <- parliament_data(election_data = ksp_2019_data,
+                                      type = "semicircle",
+                                      parl_rows = 5,
+                                      party_seats = ksp_2019_data$seats)
+
+deputies_2019 <- ggplot(ks_parliament_2019, aes(x, y, colour = party_short)) +
+  geom_parliament_seats(size=5) + 
+  #highlight the party in control of the House with a black line
+  geom_highlight_government(government == 1) +
+  #draw majority threshold
+  # draw_majoritythreshold(n = 61, label = TRUE, type = 'semicircle')+
+  #set theme_ggparliament
+  theme_ggparliament(legend=TRUE) +
+  #other aesthetics
+  labs(colour = "Partia ose Koalicionet", 
+       title = "Parlamenti i Republikes së Kosovës në vitin 2019",
+       subtitle = "Ulëset e deputetëve sipas zgjedhjeve të 2017-ës dhe koalicioneve të 2019-ës") +
+ scale_colour_manual(values = ks_parliament_2019$colour,
+                     limits = ks_parliament_2019$party_short)
+
+deputies_2019
+
+ggsave("visualization/kosovo-parliament-summarized-2019-shqip.png", width=12, height = 7)
